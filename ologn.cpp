d443d2e3d08a849e56b7c68be27ea06d4d15f10c
evangtim <evangtimugbaja@gmail.com>
2022-11-08 22:39:20 +0100

    initial commit

---
+++ ologn.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int bSearch(int arr[], int size, int item)
+{
+    int low = 0;
+    int high = size - 1;
+    int mid = low + (high - low) / 2;
+
+    while (low <= high)
+
+    {
+        mid = low + (high - low) / 2;
+        if (arr[mid] == item)
+            return mid;
+        else if (arr[mid] < item)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1; // unsuccessful result
+}
